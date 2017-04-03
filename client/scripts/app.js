3313e60250424d6ef5ef66e723bda97a3e5766e4
MKS-Student <mks.student@hackreactor.com>
2017-04-03 16:30:38 -0700

    Implement most of the methods being tested to some level
    Fetching and adding messages to the DOM are still not passing.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,59 @@
-// YOUR CODE HERE:
+var app = {};
+
+app.init = function() {
+  //stuff
+};
+
+app.send = function(message) {
+  $.ajax({
+    url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function() {
+  var fetchedData;
+  $.ajax({
+    url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    data: {},
+    contentType: 'application/json',  // ??? <-- Change?
+    success: function (data) {
+      console.log('chatterbox: Message received');
+      fetchedData = data.results;
+      console.log(fetchedData);
+      console.log('inside^^^^^^^^^^^^');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get message', data);
+    }
+  });
+        console.log('outsideeeeeee');
+  console.log(fetchedData);
+  return fetchedData;
+};
+
+app.clearMessages = function() {
+  var $chats = $('#chats');
+  $chats.empty();
+};
+
+app.renderMessage = function(message) {
+  var $chats = $('#chats');
+  console.log($chats);
+  $chats.append(message);
+};
+
+app.renderRoom = function(string) {
+  //
+};
